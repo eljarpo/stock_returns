1f51dc352c8f04cbe8f4925791da045b7ea04df6
eljarpo <jorge@eljarpo.cl>
2024-09-30 13:02:34 -0300

    menu updated

--- main.rb
+++ main.rb
@@ -74,19 +74,84 @@ class Portfolio
   end
 end
 
+class Application
+  def initialize
+    @portfolio = Portfolio.new
+    @start_date = nil
+    @end_date = nil
+    while @start_date.nil? || @end_date.nil?
+      get_dates
+    end
+    while true
+      menu
+    end
+  end
 
-@portfolio = Portfolio.new
-start_date = Date.new(2020, 1, 1)
-end_date = Date.new(2024, 1, 1)
-@portfolio.stocks.each { |stock|
+  def get_dates
+    puts "Ingresa fecha de inicio (DD/MM/YYYY)"
+    sdate = gets.chomp
+    puts "Ingresa fecha de termino (DD/MM/YYYY)"
+    edate = gets.chomp
 
-  puts stock.name
-  puts "from #{start_date} ($#{stock.price(start_date)}) to #{end_date} ($#{stock.price(end_date)})"
-  puts "#{stock.annualized_return(start_date, end_date)}%"
-  puts "----------------------"
-}
-puts "Portfolio annualized Returns"
-puts "from #{start_date}  to #{end_date}"
-puts "#{end_date.year - start_date.year} years"
-puts "#{@portfolio.annualized_return(start_date, end_date)}%"
+    sdate_a = sdate.split('/')
+    edate_a = edate.split('/')
+
+    if sdate_a.length != 3
+      @start_date = Date.new(2010, 1, 1) 
+    else
+      @start_date = Date.new(sdate_a[2].to_i, sdate_a[1].to_i, sdate_a[0].to_i)
+    end
+    if edate_a.length != 3
+      @end_date = Date.today 
+    else
+      @end_date = Date.new(edate_a[2].to_i, edate_a[1].to_i, edate_a[0].to_i)
+    end
+  end
+
+  def menu
+    puts "----------------------"
+    puts "Portfolio v0.1"
+    puts "----------------------"
+    puts "Inicio: #{@start_date}" if !@start_date.nil?
+    puts "Termino: #{@end_date}" if !@start_date.nil?
+    puts "----------------------"
+    puts "1. Calcular rendimiento"
+    puts "2. Listar acciones"
+    puts "3. Cambiar Fechas"
+    puts "4. Salir"
+    puts "----------------------"
+
+    option = gets.chomp.to_i
+    case option
+    when 1
+      run_performance
+      gets
+    when 2
+      @portfolio.stocks.each { |stock|
+        puts "#{stock.name} - $#{stock.price(Date.today)}" 
+      }
+      gets
+    when 3
+      get_dates
+    when 4
+      exit
+    end
+  end
+
+  def run_performance
+    @portfolio.stocks.each { |stock|
+
+      puts stock.name
+      puts "from #{@start_date} ($#{stock.price(@start_date)}) to #{@end_date} ($#{stock.price(@end_date)})"
+      puts "#{stock.annualized_return(@start_date, @end_date)}%"
+      puts "----------------------"
+    }
+    puts "Portfolio annualized Returns"
+    puts "from #{@start_date}  to #{@end_date}"
+    puts "#{@end_date.year - @start_date.year} years"
+    puts "#{@portfolio.annualized_return(@start_date, @end_date)}%"
+  end
+end
+
+Application.new
 
